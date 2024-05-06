cfc2523e22d11f51b44a87487f1ab01fdbc0da78
TheMegax <mathias.tello12@gmail.com>
2024-05-06 17:51:08 -0400

    Added sound search routine

--- src/main/java/net/themegax/eventful/EventfulMain.java
+++ src/main/java/net/themegax/eventful/EventfulMain.java
@@ -5,12 +5,18 @@ import net.citizensnpcs.api.event.CitizensEnableEvent;
 import net.citizensnpcs.api.npc.NPC;
 import net.themegax.eventful.Database.DatabaseManager;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,8 +24,17 @@ public final class EventfulMain extends JavaPlugin {
 
     private static Logger logger;
     private static DatabaseManager databaseManager;
-    private static NPC citizenNPC;
+    private static Boolean isCitizensEnabled = false;
+    private static final Dictionary<NPC, PatrolProperties> PatrolDict = new Hashtable<>();
     FileConfiguration config = getConfig();
+
+    private static class PatrolProperties {
+        public float susLevel = 0;
+        public Location lastKnownPlayerLocation = null;
+        public Location closestSoundLocation = null;
+
+        public PatrolProperties() {}
+    }
 
     @Override
     public void onEnable() {
@@ -44,7 +59,7 @@ public final class EventfulMain extends JavaPlugin {
             Bukkit.getServer().getPluginManager().registerEvents(new Listener() {
                 @EventHandler
                 public void onCitizensEnable(CitizensEnableEvent ev) {
-                    citizenNPC = CitizensAPI.getNPCRegistry().getById(1);
+                    isCitizensEnabled = true;
                 }
             }, this);
         }
@@ -64,23 +79,89 @@ public final class EventfulMain extends JavaPlugin {
     }
 
     public static void onNPCTick() {
-        // Find the closest player and chase them.
-        var minDist = 99999.0;
-        Player closestPlayer = null;
-        var npcLoc = citizenNPC.getEntity().getLocation();
-        for (var player : Bukkit.getOnlinePlayers()) {
-            var dist = player.getPlayer().getLocation().distance(npcLoc);
-            if (dist > minDist) continue;
-            
-            minDist = dist;
-            closestPlayer = player;
-        }
-        
-        if (closestPlayer != null) {
-            citizenNPC.getEntity().setSilent(true);
-            citizenNPC.getNavigator().setTarget(closestPlayer.getLocation());
-            citizenNPC.getNavigator().getLocalParameters().range(1000);
-            citizenNPC.getNavigator().getLocalParameters().speedModifier(2);
+        if (!isCitizensEnabled) return;
+
+        for ( NPC npc : CitizensAPI.getNPCRegistry()) {
+            var npcEntity = (LivingEntity) npc.getEntity();
+            if (npcEntity == null) continue;
+
+            // We control NPCs with the "patrol" tag
+            // If the NPC is not a villager, auto convert it to one
+            if (!npcEntity.getScoreboardTags().contains("patrol")) continue;
+            if (npcEntity.getType() != EntityType.VILLAGER) {
+                npc.setBukkitEntityType(EntityType.VILLAGER);
+                npcEntity = (LivingEntity) npc.getEntity();
+                npcEntity.addScoreboardTag("patrol");
+            }
+            npcEntity.setSilent(true); // Just so we don't have to deal with random villager sounds
+
+            // Get patrol NPC properties
+            PatrolProperties properties = PatrolDict.get(npc);
+            if (properties == null) { // Setup if needed
+                properties = new PatrolProperties();
+                PatrolDict.put(npc, properties);
+            }
+
+            // Find the closest player
+            var minDist = 99999.0;
+            Player closestPlayer = null;
+            for (var player : Bukkit.getOnlinePlayers()) {
+                var dist = player.getPlayer().getLocation().distance(npcEntity.getLocation());
+                if (dist > minDist) continue;
+                minDist = dist;
+                closestPlayer = player;
+            }
+            if (closestPlayer == null) continue;
+
+            // << Here is the meat of the chase routine >> //
+
+            // Check if the NPC can see the player
+            var world = closestPlayer.getWorld();
+            var playerDir = closestPlayer.getEyeLocation().subtract(npcEntity.getEyeLocation()).toVector();
+            var traceResult = world.rayTraceBlocks(npcEntity.getEyeLocation(), playerDir, 100f);
+            if (traceResult != null && traceResult.getHitBlock() == null) { // This means it has direct line of sight with the player
+                properties.susLevel = Math.min(properties.susLevel + 0.01f, 1); // Takes 5 seconds to max out the sus meter
+                GetLogger().log(Level.INFO, properties.susLevel + "");
+                if (properties.susLevel >= 1) {
+                    properties.lastKnownPlayerLocation = closestPlayer.getLocation();
+                }
+            }
+
+            // Check for audio entities near the npc
+            minDist = 99999.0;
+            Entity closestSoundEntity = null;
+            for (var entity : npcEntity.getNearbyEntities(16, 16, 16)) {
+                if (!entity.getScoreboardTags().contains("audio")) continue; // Check for the "audio" tag
+                var dist = entity.getLocation().distance(npcEntity.getLocation());
+                if (dist > minDist) continue;
+                minDist = dist;
+                closestSoundEntity = entity;
+            }
+            if (closestSoundEntity != null)  {
+                properties.closestSoundLocation = closestSoundEntity.getLocation();
+            }
+
+            float speedModifier = 1f;
+            Location targetLocation = null;
+
+            // If the npc can hear a sound, but not locate the player
+            if (properties.closestSoundLocation != null && properties.lastKnownPlayerLocation == null) {
+                speedModifier = 1f;
+                targetLocation = properties.closestSoundLocation;
+            }
+            // Else if the player is being tracked
+            else if (properties.lastKnownPlayerLocation != null) {
+                speedModifier = 1.4f;
+                targetLocation = properties.lastKnownPlayerLocation;
+            }
+
+            // Move towards the target location
+            if (properties.closestSoundLocation != null && properties.lastKnownPlayerLocation != null) {
+                npc.getNavigator().setTarget(targetLocation);
+                npc.getNavigator().getLocalParameters().range(500);
+                npc.getNavigator().getLocalParameters().speedModifier(speedModifier);
+                npc.getNavigator().getLocalParameters().pathDistanceMargin(0.1f);
+            }
         }
     }
 }
